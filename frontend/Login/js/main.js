c20ca29b9fb130ed77490239b822fd8d61bf9381
Ana <anaconradof@gmail.com>
2023-12-03 23:15:14 -0300

    fix: rotas e backend

--- frontend/Login/js/main.js
+++ frontend/Login/js/main.js
@@ -1,57 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var formLogin = document.getElementById('login-form');
 
-(function ($) {
-    "use strict";
+    formLogin.addEventListener('submit', function(e){
+        e.preventDefault();
+        
+        var email = document.querySelector('input[name="email"]').value;
+        var senha = document.querySelector('input[name="senha"]').value;
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, senha }) 
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na resposta do servidor');
             }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+            return response.json();
+        })
+        .then(data => {
+            if (data.message === 'Login bem-sucedido') {
+                window.location.href = 'http://127.0.0.1:5500/WEB2_2023-2/Trelo/'; 
+            } else {
+                alert(data.message); 
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao enviar formulário:', error);
         });
-    });
+    })
+});
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
+/*
+
+var prefixoImagens = '../../Personagem/'
+
+var customizationOptions = {
+    cabelo: [prefixoImagens + '/Cabelo/cabeloLoiro.png', prefixoImagens + '/Cabelo/cabeloPreto.png', prefixoImagens + '/Cabelo/cabeloCastanho.png'],
+    pele: [prefixoImagens + '/Pele/peleClara.png', prefixoImagens + '/Pele/peleMedia.png', prefixoImagens + '/Pele/peleEscura.png'],
+    olho: [prefixoImagens + 'Olho/olho.png'],
+    planoDeFundo: [prefixoImagens + '/Tela Batalha Chuvosa.jpg', '../../Personagem/Tela Batalha Nublado.jpg']
+};
+
+var currentOptionIndexes = {
+    cabelo: 0, 
+    pele: 0, 
+    olho: 0, 
+    planoDeFundo: 0
+};
+
+function loadCharacterConfiguration() {
+    for (var part in currentOptionIndexes) {
+        if (currentOptionIndexes.hasOwnProperty(part)) {
+            var savedIndex = localStorage.getItem(part);
+            if (savedIndex) {
+                currentOptionIndexes[part] = parseInt(savedIndex, 10); // Parse the saved index as a base-10 integer
+                var partImage = document.getElementById(part + '-image');
+                if (partImage) {
+                    partImage.src = customizationOptions[part][currentOptionIndexes[part]]; // Set the image source
+                }
             }
         }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
     }
+}
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
+document.addEventListener('DOMContentLoaded', function() {
+    loadCharacterConfiguration();
+});
+*/ 
